dc3a2d2f4ea9ef66eaae858199870dc5a3891247
zlydenko <anton.zlydenko@gmail.com>
2018-03-03 11:20:05 +0200

    feat: add lazy load restaurants section && dns-prefetch AWS CDN with images

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,9 @@
 (function() {
   const btn = document.querySelector(".scroll-to-top");
+  const restaurantsSection = document.querySelector(".restaurants");
+  const spinner = document.querySelector(".spinner");
   const startingPoint = 350;
+
   window.onscroll = function() {
     scrollFunction();
   };
@@ -22,5 +25,12 @@
     document.documentElement.scrollTop = 0;
   }
 
+  window.addEventListener("load", function() {
+    spinner.style.display = "none";
+    spinner.style.visibility = "hidden";
+    restaurantsSection.classList.add("visible-animate");
+    restaurantsSection.style.display = "block";
+    restaurantsSection.style.visibility = "visible";
+  });
   btn.addEventListener("click", topFunction);
 })();
